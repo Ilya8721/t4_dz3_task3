bb62b72cb6340db3c6056ca38ad74a691415ded6
Ilya Bobrov <i.bobrov87@gmail.com>
2023-02-11 18:28:40 +0300

    variables changed

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
-        double s = 1_000_000;
-        int n = 12 ;
-        double ig = 9.99;
-        int payment = service.calculate(s, n, ig);
+        double amountOfCredit = 1_000_000;
+        int creditTerm = 12 ;
+        double annualInterestRate = 9.99;
+        int payment = service.calculate(amountOfCredit, creditTerm, annualInterestRate);
         System.out.println(payment);
     }
 }
